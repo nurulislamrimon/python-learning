2d85cca0a0c9c85627febdcf502524c4c27c7adf
nurulislamrimon <nurulislamrimon@gmail.com>
2025-06-24 23:03:22 +0600

    Template string alternative in python added

--- built_in_fn.py
+++ built_in_fn.py
@@ -1,4 +1,9 @@
-name = "Nurul Islam Rimon"
+fname = "Nurul Islam"
+
+lname = "Rimon"
+
+name = F"{fname} {lname}"
+
 
 # length function
 length = len(name)
@@ -9,3 +14,4 @@ print("Name zero index letter: ", name[0])
 print("Name one index letter: ", name[-1])
 print("Name zero to three index letter: ", name[0:3])
 print("Name from a index to end index letter: ", name[3:])
+print("Full name: ", name[:])
