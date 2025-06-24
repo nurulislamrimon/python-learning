e5e79505ceeab1916637f2fc2d6d50ff4abe60ab
nurulislamrimon <nurulislamrimon@gmail.com>
2025-06-24 22:42:47 +0600

    Multi line variable added

--- variable.py
+++ variable.py
@@ -1,6 +1,11 @@
-name="Nurul Islam Rimon"
-age=26
-is_married=False
+name = "Nurul Islam Rimon"
+multi_line = """
+Hi Rasel,
+This is Nurul!
+"""
+age = 26
+is_married = False
 
 
-print("Name: "+name,"Age: ",age,"Marital status: ",is_married)
+print("Name: " + name, "Age: ", age, "Marital status: ",
+      is_married, "Descriptions: " + multi_line)
